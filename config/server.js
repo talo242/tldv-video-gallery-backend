b5244af969856edcbdcf88255c25787e233ea5f9
Ricardo Velasco <talo242@gmail.com>
2021-01-31 17:49:24 -0600

    ~ Initial commit ~

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e49e3b2b61209463174a79fe08c26340'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2635371e-8663-40d3-aa09-58f0891d8f85'
+};
